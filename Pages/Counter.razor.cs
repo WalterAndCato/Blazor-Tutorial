b2b3461b6882dc011a34dde77b42a5bde2f1a0a5
Peter Steinkamp <peter.steinkamp@googlemail.com>
2020-05-01 18:59:23 +0200

    33 - Ref directive Making References of Components

--- Pages/Counter.razor.cs
+++ Pages/Counter.razor.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using BlazorServerApp2.Shared.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorServerApp2.Pages
 {
@@ -11,25 +13,26 @@ namespace BlazorServerApp2.Pages
         [Inject] private TransientService transient { get; set; }
         
         private int currentCount = 0;
+        private static int currentCountStatic = 0;
 
         private void IncrementCount()
         {
+            currentCountStatic++;
             currentCount++;
             transient.Value = currentCount;
             singleton.Value = currentCount;
         }
-    
-        private List<Movie> movies;
+
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
+        }
+        
  
         protected override void OnInitialized()
         {
             base.OnInitialized();
-            movies = new List<Movie>
-            {
-                new Movie() {Title = "<b>Krieg der Sterne Reloaded</b>", ReleaseDate = new DateTime(1977, 7, 4)},
-                new Movie() {Title = "<i>Das Imperium schlägt zurück</i>", ReleaseDate = new DateTime(1977, 7, 4)},
-                new Movie() {Title = "Rückkehr der Jedi-Ritter", ReleaseDate = new DateTime(1977, 7, 4)},
-            };
         }
     }
 }
